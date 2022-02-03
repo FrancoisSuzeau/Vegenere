604e580d75187e545d9732227d710b776bdc23e9
FrancoisSuzeau <frsuzeau@gmail.com>
2022-02-03 14:12:00 +0100

    Preparing part two of the project

---
+++ src/BabageKasiki/Analysis.hpp
@@ -0,0 +1,27 @@
+/*  Author : SUZEAU François
+
+    Creation : 03/02/2022
+
+    Purpose : Module of Exercice 6 for Part Two
+*/
+
+#ifndef ANALYSIS_H
+#define ANALYSIS_H
+
+    #include <iostream>
+    
+
+    class Analysis {
+
+
+        private:
+
+            
+
+        public:
+
+            Analysis();
+            ~Analysis();
+    };
+
+#endif
